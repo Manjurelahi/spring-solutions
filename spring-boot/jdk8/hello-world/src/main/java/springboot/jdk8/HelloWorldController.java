132cb6d3ec284146e5e53dfcb5dbcaf8ee12de32
Manjurelahi Patel <manjurelahi.patel@gmail.com>
2024-03-21 21:21:24 +0530

    [spring-solutions] Creates Hello World Spring Boot application based on JDK8

---
+++ spring-boot/jdk8/hello-world/src/main/java/springboot/jdk8/HelloWorldController.java
@@ -0,0 +1,13 @@
+package springboot.jdk8;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+    @RequestMapping("/")
+    public String helloWorld() {
+        return "Hello World!";
+    }
+}
